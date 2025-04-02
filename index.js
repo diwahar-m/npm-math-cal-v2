a85c659606ce0282a06af8d2d7f9f61d036de355
diwahar-m <msdiwahar5@gmail.com>
2025-04-02 18:09:02 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+
+function checkType(a, b){
+    if(typeof a === 'number' && typeof b === 'number'){
+        return true
+    }
+    return false
+}
+
+const inValidNumber  = "Invalid inputs"
+
+function addition(a, b){
+    if(checkType(a,b)){
+        return a + b;
+    }
+    return inValidNumber;
+}
+function subtraction(a, b){
+     if(checkType(a,b)){
+        return a - b;
+    }
+    return inValidNumber;
+}
+function multiplication(a, b){
+     if(checkType(a,b)){
+        return a * b;
+    }
+    return inValidNumber;
+}
+
+function division(a, b){
+     if(checkType(a,b)){
+        return a / b;
+    }
+    return inValidNumber;
+}
+
+
+export default {addition, subtraction, multiplication, division};
